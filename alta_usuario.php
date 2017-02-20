2cb4e56946e8c7fc92e8d7faa82cc56572879c88
Sergi <sergitnsr@gmail.com>
2017-02-20 16:58:25 +0100

    Pruebas logo
    Pruebas

--- alta_usuario.php
+++ alta_usuario.php
@@ -21,7 +21,7 @@
 
 <body>
  
-  <h1 style="color:#43A047;">Registro en  MyContacts</h1>
+  <h1 style="color:#43A047;">VOTE IT</h1>
   
            
 
@@ -29,7 +29,7 @@
       <li><label>Nombre de usuario: </label><input type="text" id="nombre" name="nombre" placeholder="usuario" required onfocus="document.Login.nombre.style.color='';" /></li>
       <li><label>Contraseña: </label><input type="password" id="password" name="password" placeholder="contraseña" required onfocus="document.Login.password.style.color='';"/></li>
      <li><label for="email">Email: </label> <input type="email" name="email" placeholder="email@email.com" required /> <span class="form_hint">Formato correcto: "name@something.com"</span> </li>
-     <li><label>Tu foto: </label><input id="foto" name="foto" type="file" ></li>
+     <!-- <li><label>Tu foto: </label><input id="foto" name="foto" type="file" ></li> -->
       <button>Registrarse</button>
    
     </form>
